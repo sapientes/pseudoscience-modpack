1c0e454ca825e30d51ae647638b0e8ea24bc4a24
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2025-08-03 17:37:41 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,26 +1,12 @@
-{pkgs ? import <nixpkgs> {}}: let
-  tombl = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "tombl";
-    version = "0.2.3";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "snyball";
-      repo = pname;
-      rev = "v${version}";
-      hash = "sha256-XHvAgJ8/+ZkBxwZpMgaDchr0hBa1FXAd/j1+HH9N6qw=";
-    };
-
-    cargoHash = "sha256-3hgfAyMydSHnHqPLUQDR/5OWS4GUGeYsJNCX1TDN/wA=";
-  };
-in
-  pkgs.mkShellNoCC {
-    packages = with pkgs; [
-      wget
-      tombl
-      packwiz
-      just
-      envsubst
-      ripgrep
-      zip
-    ];
-  }
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    wget
+    tombl
+    packwiz
+    just
+    envsubst
+    ripgrep
+    zip
+  ];
+}
